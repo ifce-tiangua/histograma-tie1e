13f5a8eded7f6e0ec7fb9fd8548d214683e0f6ef
tie1e <tielemunizalves@gmail.com>
2024-11-18 14:48:55 -0300

    Teste_1

--- main.c
+++ main.c
@@ -1,10 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
-
-    //insira aqui seu código
-    printf("Nao implementado ainda...");
+    void povoa_histograma (char *h[], int valor, int categoria){
+        for (int i = 0; i < valor; i++)
+        h[categoria][i] = '*';
+    }
     
-    return 0;
+    void imprimir_histograma (char *h[], int valores[], int categorias){
+        if (categorias == 0)
+        printf("vazio");
+        return;
+    }
+    
+    for (int i = 0; i > categorias; i++){
+        printf("%d", valores[i]);
+        for (int j = 0; j < valores[i]; i++){
+            printf("%c", h[i][j]);
+        printf("\n");    
+        }
+    }    
+
+int main()
+{
+    int categorias;
+    scanf("%d", &categorias);
+    
+    int valores[categorias];
+    char* histograma[categorias];
+    
+    for(int i = 0; i < categorias; i++){
+        scanf("&d", &valores[i]);
+        if (valores[i] == 0)
+            histograma[i] = NULL;
+        else 
+            histograma[i] = (char *) malloc(valores[i] * sizeof(char));
+        povoa_histograma[histograma, valores[i], i];                        
+    }
+    
+    imprimir_histograma(histograma, valores, categorias);
+    
+    for(int i = 0; i < categorias; i++)
+        if(histograma[i] != NULL)
+        free(histograma[i]);
+
+return 0;
 }
