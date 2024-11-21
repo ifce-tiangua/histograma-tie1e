ff79b76db0f77cbfb00c5e9c93cd426449d540be
tie1e <tielemunizalves@gmail.com>
2024-11-21 12:51:30 -0300

    Teste_2

--- main.c
+++ main.c
@@ -5,40 +5,40 @@
         for (int i = 0; i < valor; i++)
         h[categoria][i] = '*';
     }
-    
+
     void imprimir_histograma (char *h[], int valores[], int categorias){
-        if (categorias == 0)
+        if (categorias == 0){
         printf("vazio");
         return;
-    }
-    
-    for (int i = 0; i > categorias; i++){
-        printf("%d", valores[i]);
-        for (int j = 0; j < valores[i]; i++){
-            printf("%c", h[i][j]);
-        printf("\n");    
         }
-    }    
+
+    for (int i = 0; i < categorias; i++){
+        printf("%d", valores[i]);
+        for (int j = 0; j < valores[i]; j++)
+            printf("%c", h[i][j]);
+        printf("\n");
+    }
+    }
 
 int main()
 {
     int categorias;
     scanf("%d", &categorias);
-    
+
     int valores[categorias];
     char* histograma[categorias];
-    
+
     for(int i = 0; i < categorias; i++){
         scanf("&d", &valores[i]);
         if (valores[i] == 0)
             histograma[i] = NULL;
-        else 
+        else
             histograma[i] = (char *) malloc(valores[i] * sizeof(char));
-        povoa_histograma[histograma, valores[i], i];                        
+        povoa_histograma(histograma, valores[i], i);
     }
-    
+
     imprimir_histograma(histograma, valores, categorias);
-    
+
     for(int i = 0; i < categorias; i++)
         if(histograma[i] != NULL)
         free(histograma[i]);
